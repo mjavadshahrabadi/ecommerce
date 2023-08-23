a3f73cb430ddea6dc82d59758be834b24392998d
mjavadshahrabadi <mjavadshahrabadi@gmail.com>
2023-08-23 11:30:45 +0330

    responsive benefits part

--- src/components/home/Benefits/BenefitsItem.tsx
+++ src/components/home/Benefits/BenefitsItem.tsx
@@ -12,13 +12,17 @@ export const BenefitsItem: FC<BenefitsItemProps> = ({
   title,
 }): ReactElement => {
   return (
-    <li className="flex items-center justify-between space-x-2">
-      <div className="flex flex-col space-y-1 items-end">
-        <h3 className="text-gray-800 font-semibold text-[14px]">{title}</h3>
-        <h5 className="text-gray-400 text-[12px] font-semibold">{subtitle}</h5>
-      </div>
-      <div className="border p-4 rounded-md group hover:bg-primary hover:border-primary cursor-pointer duration-300 ease-out">
+    <li className="flex flex-col lg:flex-row items-center justify-center">
+      <div className="border p-4 rounded-md group hover:bg-primary hover:border-primary cursor-pointer duration-300 ease-out ml-2">
         {children}
+      </div>
+      <div className="flex flex-col space-y-1 my-4">
+        <h3 className="text-gray-800 font-semibold text-[14px] text-center lg:text-right">
+          {title}
+        </h3>
+        <h5 className="text-gray-400 text-[12px] font-semibold text-center lg:text-right">
+          {subtitle}
+        </h5>
       </div>
     </li>
   )
--- src/components/home/Benefits/index.tsx
+++ src/components/home/Benefits/index.tsx
@@ -4,13 +4,16 @@ import { BenefitsItem } from '@/components/home/Benefits/BenefitsItem'
 
 export const Benefits: FC = (): ReactElement => {
   return (
-    <ul className="mt-8 max-w-8xl mx-auto flex flex-row-reverse items-center justify-between">
+    <ul
+      className="mt-5 lg:mt-8 max-w-8xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-4 items-center justify-between px-4 2xl:px-0"
+      dir="rtl"
+    >
       <BenefitsItem title="ضمانت اصالت کالا" subtitle="ضمانت اصالت کالا">
         <PatchCheck className="text-primary w-8 h-8 group-hover:text-white duration-300 ease-out" />
       </BenefitsItem>
 
       <BenefitsItem
-        title="رسال رایگان بالای ۵۰۰,۰۰۰ تومان"
+        title="ارسال رایگان بالای ۵۰۰,۰۰۰ تومان"
         subtitle="برای سراسر کشور"
       >
         <Truck className="text-primary w-8 h-8 group-hover:text-white duration-300 ease-out" />
